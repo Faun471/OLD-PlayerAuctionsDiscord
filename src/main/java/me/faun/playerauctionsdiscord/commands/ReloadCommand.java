3cf6fed2fedffb07f70b66f1ab0429ee45e846c4
Faun471 <bearbrand949@gmail.com>
2021-09-16 10:26:17 +0800

    Added tab completion cause why not

--- src/main/java/me/faun/playerauctionsdiscord/commands/ReloadCommand.java
+++ src/main/java/me/faun/playerauctionsdiscord/commands/ReloadCommand.java
@@ -5,20 +5,50 @@ import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.command.TabCompleter;
+import org.bukkit.entity.Player;
+import org.bukkit.util.StringUtil;
 
-public class ReloadCommand implements CommandExecutor {
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class ReloadCommand implements CommandExecutor, TabCompleter {
+
+    List<String> results = new ArrayList<>();
 
     //Todo this command looks dum, I'll probs change it later
     @Override
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
         if (label.equalsIgnoreCase("playerauctionsdiscord") || label.equalsIgnoreCase("pad")){
-            if (args[0].equalsIgnoreCase("reload")){
+            if (args[0].equalsIgnoreCase("reload") && args.length == 1){
                 PlayerAuctionsDiscord.getInstance().reloadConfig();
-                sender.sendMessage(ChatColor.GREEN + PlayerAuctionsDiscord.getInstance().getDescription().getName() + " reloaded");
+                if (sender instanceof Player) sender.sendMessage(ChatColor.GREEN + PlayerAuctionsDiscord.getInstance().getDescription().getName() + " reloaded");
+                else PlayerAuctionsDiscord.getInstance().getLogger().info(PlayerAuctionsDiscord.getInstance().getDescription().getName() + " reloaded");
             }
             else sender.sendMessage(ChatColor.translateAlternateColorCodes('&',"&cUsage /playerauctionsdiscord reload"));
             return true;
         }
         return false;
+    }
+
+    @Override
+    public List <String> onTabComplete(CommandSender sender, Command cmd, String label, String[] args) {
+        if (args.length == 1) {
+            if (cmd.getLabel().equalsIgnoreCase("playerauctionsdiscord") || cmd.getLabel().equalsIgnoreCase("pad")) {
+                results.clear();
+                results.add("reload");
+            }
+        }
+        return sortedResults(args[0]);
+    }
+
+    public List <String> sortedResults(String arg) {
+        final List <String> completions = new ArrayList <> ();
+        StringUtil.copyPartialMatches(arg, results, completions);
+        Collections.sort(completions);
+        results.clear();
+        results.addAll(completions);
+        return results;
     }
 }
